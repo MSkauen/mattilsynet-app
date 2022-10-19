52f2af6812c41ed9dcb1b55b12649785d3148c9a
Mskauen <mathias.skauen.harestad@gmail.com>
2022-10-19 03:03:54 +0200

    CSS styling

--- src/client/RegistrerForflytning.jsx
+++ src/client/RegistrerForflytning.jsx
@@ -40,62 +40,64 @@ function RegistrerForflytning({ data }) {
       <header id="head" className="RegistrerForflytning-header">
         <img id="logo" src={logo} className="RegistrerForflytning-logo" alt="logo" />
         <div id="form" className="RegistrerForflytning-form">
-        <form onSubmit={handleForflytning}>
-          <br/>
-          <label>Identifiser dyrehold:
-          <select
-            required
-            name="dyrehold" 
-            defaultValue=""
-            onChange={handleChange}
-          >
-            <option value="" disabled>Velg dyrehold ...</option>
-            <option value="2512341">Dyrehold 1</option>
-            <option value="2512342">Dyrehold 2</option>
-            <option value="2512343">Dyrehold 3</option>
-            <option value="2512344">Dyrehold 4</option>
-          </select>
-          <br/>
-          </label>
-          <label>Individ merke(r):
-            <input
-              required
-              type="number" 
-              name="fra" 
-              value={inputs.fra || "fra"} 
-              onChange={handleChange}
-              style={{width: 80 + 'px'}}
-            />
-            &nbsp;
-            <input
-              required
-              type="number" 
-              name="til" 
-              value={inputs.til || "til"} 
-              onChange={handleChange}
-              style={{width: 80 + 'px'}}
-            />
-          </label>
-          <br/>
-          <label>Produksjonsplass:
+        <form className="submitForm" onSubmit={handleForflytning}>
+          <div className="col">
+            <label>Identifiser dyrehold:</label>
             <select
-              id="sted"
+              className="formInput"
               required
-              name="sted"
+              name="dyrehold" 
+              defaultValue=""
               onChange={handleChange}
-              defaultValue="">
-
-            {/*oppgave 3a: Fyll inn verdier fra kall til http://localhost:8080/produksjonsplassregister*/}
-
-              <option value="" disabled>Velg produksjonsplass ...</option>
-              {data.map(b => (
-                  <option key={b.produksjonsplassid} value={b.produksjonsplassid}>{b.kommunenummer}-{b.gaardsnummer}/{b.bruksnummer}</option>
-              ))}
-
+            >
+              <option value="" disabled>Velg dyrehold ...</option>
+              <option value="2512341">Dyrehold 1</option>
+              <option value="2512342">Dyrehold 2</option>
+              <option value="2512343">Dyrehold 3</option>
+              <option value="2512344">Dyrehold 4</option>
             </select>
-          </label>
-          <br/>
-          <input type="submit" name="registrer" disabled={submitting}/>
+          </div>
+          <div className="col">
+            <label>Individ merke(r):</label>
+            <div className="row">
+              <input
+                className="formInput"
+                required
+                type="number" 
+                name="fra"
+                placeholder="fra"
+                value={inputs.fra || "fra"} 
+                onChange={handleChange}
+              />
+              <input
+                className="formInput"
+                required
+                type="number" 
+                name="til"
+                placeholder="til"
+                value={inputs.til || "til"} 
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="col">
+            <label>Produksjonsplass:</label>
+              <select
+                className="formInput"
+                id="sted"
+                required
+                name="sted"
+                onChange={handleChange}
+                defaultValue="">
+
+                <option value="" disabled>Velg produksjonsplass ...</option>
+                {data.map(b => (
+                    <option key={b.produksjonsplassid} value={b.produksjonsplassid}>{b.kommunenummer}-{b.gaardsnummer}/{b.bruksnummer}</option>
+                ))}
+
+              </select>
+          </div>
+          <input className="formButton" type="submit" name="registrer" disabled={submitting}/>
         </form>
         </div>
       </header>
